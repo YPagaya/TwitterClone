2e2ccb045b929bc2ee8b58f7ca332639b17057c0
YPagaya <7418536@ed.tus.ac.jp>
2020-03-18 22:48:34 +0900

    投稿を表示するところまで編集

--- t_clone/post/models.py
+++ t_clone/post/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone 
 
-# Create your models here.
+
+class post(models.Model):
+    post_text = models.CharField(max_length=140)
+    post_date = models.DateTimeField('date posted')
+    
+    def __str__(self):
+        return self.post_text
--- t_clone/post/views.py
+++ t_clone/post/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import post
+from django.utils import timezone
 
-# Create your views here.
+#Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the post index.")
+
+def post_list(request):
+    posts = post.objects.filter(post_date__lte=timezone.now()).order_by('post_date')
+    return render(request, 'post/post_list.html', {'posts': posts})
+
+'''def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+'''
