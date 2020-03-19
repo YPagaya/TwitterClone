fd0ba9680d375816a7a787941b9877e6d2743939
YPagaya <7418536@ed.tus.ac.jp>
2020-03-19 12:56:04 +0900

    投稿フォームから投稿

--- t_clone/post/views.py
+++ t_clone/post/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import post
 from django.utils import timezone
+from .forms import PostForm
+
 
 #Create your views here.
 def index(request):
@@ -11,7 +13,23 @@ def post_list(request):
     posts = post.objects.filter(post_date__lte=timezone.now()).order_by('post_date')
     return render(request, 'post/post_list.html', {'posts': posts})
 
+def post_detail(request, pk):
+    posts = get_object_or_404(post, pk=pk)
+    return render(request, 'post/post_detail.html', {'posts': posts})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.post_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'post/post_edit.html', {'form': form})
+
 '''def post_new(request):
     form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'post/post_edit.html', {'form': form})
 '''
